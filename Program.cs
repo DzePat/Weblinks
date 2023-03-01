1a7f3d2d141e24fd8981b4e84d4e6b96595c5f24
Tomas Kindahl <tomas.kindahl@bahnhof.se>
2023-03-01 20:59:55 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace WebLinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintWelcome();
+            string command;
+            do
+            {
+                Console.Write(": ");
+                command = Console.ReadLine();
+                if (command == "quit")
+                {
+                    Console.WriteLine("Good bye!");
+                }
+                else if (command == "help")
+                {
+                    WriteTheHelp();
+                }
+                else if (command == "load")
+                {
+                    NotYetImplemented(command);
+                }
+                else if (command == "open")
+                {
+                    NotYetImplemented(command);
+                }
+                else
+                {
+                    Console.WriteLine($"Unknown command '{command}'");
+                }
+            } while (command != "quit");
+        }
+
+        private static void NotYetImplemented(string command)
+        {
+            Console.WriteLine($"Sorry: '{command}' is not yet implemented");
+        }
+
+        private static void PrintWelcome()
+        {
+            Console.WriteLine("Hello and welcome to the ... program ...");
+            Console.WriteLine("that does ... something.");
+            Console.WriteLine("Write 'help' for help!");
+        }
+
+        private static void WriteTheHelp()
+        {
+            string[] hstr = {
+                "help  - display this help",
+                "load  - load all links from a file",
+                "open  - open a specific link",
+                "quit  - quit the program"
+            };
+            foreach (string h in hstr) Console.WriteLine(h);
+        }
+    }
+}
